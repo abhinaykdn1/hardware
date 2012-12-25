1da884d2b41c4799e838fcfe93348c5cf18e1e21
lim <lim@Lims-MacBook-Pro.local>
2012-12-25 18:00:40 +0200

    Update

--- AVR/sixrgbled/sixrgbled.c
+++ AVR/sixrgbled/sixrgbled.c
@@ -19,6 +19,11 @@ void init(void)
 	DDRB |= bv(0) | bv(1) | bv(2);
 	DDRC |= bv(0) | bv(1) | bv(2);
 	DDRD |= bv(5) | bv(6) | bv(7);
+
+	DDRB |= 0xff;
+	DDRC |= 0xff;
+	DDRD |= 0xff;
+
 }
 
 void allon(void)
@@ -52,6 +57,14 @@ void bitoff(unsigned char bits)
 	PORTD |= pd;
 }
 
+void bitset(unsigned char bits)
+{
+	bitoff(~bits);
+	biton(bits);
+
+}
+
+
 void colon(unsigned char bits) // 1-green, 2-blue, 4-red
 {
 	unsigned char pb = bits & 0x07;
@@ -64,54 +77,67 @@ void coloff(unsigned char bits)
 	PORTB &= ~pb;
 }
 
+void colset(unsigned char bits)
+{
+	coloff(~bits);
+	colon(bits);
+}
+
 int main(void)
 {
+
+//	char r[6] = {0x00, 0x10, 0x20, 0x40, 0x80, 0xf0};
+//	char g[6] = {0x00, 0x10, 0x20, 0x40, 0x80, 0xf0};
+//	char b[6] = {0x00, 0x10, 0x20, 0x40, 0x80, 0xf0};
+
+	char r[6] = {0xff, 0xff, 0xA0, 0x00, 0x00, 0x00};
+	char g[6] = {0x00, 0x10, 0xA0, 0xff, 0xA0, 0x00};
+	char b[6] = {0x00, 0x00, 0x00, 0x00, 0xA0, 0xff};
 
 	init();
 	alloff();
 
+	char ir = 0;
+	char ig = 0;
+	char ib = 0;
+
 	while (1)
 	{
-		//allon();
 
-		//for (unsigned int i=0; i<4000; i++) {};
-
-		//alloff();
-
-		//for (unsigned int i=0; i<40000; i++) {};
-		unsigned int rnd = rand();
-
-		unsigned char con = rnd & 0x0007;
-		unsigned char bon = (rnd & 0x01F8) >> 3;
-		coloff(~con);
-		colon(con);
-		bitoff(~bon);
-		biton(bon);
-
-		for (unsigned int i=0; i<4000; i++) {};
+		colset(1);
+		for (unsigned char i = 0; i<255; i++) {
+			unsigned char bits = 0;
+			for (unsigned char j = 0; j<6; j++) {
+				bits = bits << 1;
+				bits |= (g[j]>i);
+				g[j]+=ig;
+			}
+			bitset(bits);
+		}
 		alloff();
 
-		for (unsigned int i=0; i<40000; i++) {};
-//		biton(0x02);
+		colset(2);
+		for (unsigned char i = 0; i<255; i++) {
+			unsigned char bits = 0;
+			for (unsigned char j = 0; j<6; j++) {
+				bits = bits << 1;
+				bits |= (b[j]>i);
+				b[j]+=ib;
+			}
+			bitset(bits);
+		}
+		alloff();
 
-//		for (unsigned int i=0; i<40000; i++) {};
-		
-//		biton(0x04);
-
-//		for (unsigned int i=0; i<40000; i++) {};
-
-//		biton(0x08);
-
-//		for (unsigned int i=0; i<40000; i++) {};
-
-//		biton(0x10);
-
-//		for (unsigned int i=0; i<40000; i++) {};
-
-//		biton(0x20);
-
-//		for (unsigned int i=0; i<40000; i++) {};
-
-
+		colset(4);
+		for (unsigned char i = 0; i<255; i++) {
+			unsigned char bits = 0;
+			for (unsigned char j = 0; j<6; j++) {
+				bits = bits << 1;
+				bits |= (r[j]>i);
+				r[j]+=ir;
+			}
+			bitset(bits);
+		}
+		alloff();
 	}
 }
